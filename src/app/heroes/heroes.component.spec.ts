00fcde31daf3d347a5b1a733d1c6bec0664bb484
tumit <tumit@live.com>
2018-07-12 18:04:25 +0700

    finish 'Display a Heroes List'

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -1,50 +1,125 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, fakeAsync, flush, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
-import { HeroesComponent } from './heroes.component';
+import { DebugElement } from '../../../node_modules/@angular/core';
+import { HEROES } from '../heroes.mock';
 import { SharedModule } from '../shared.module';
+import { HeroesComponent } from './heroes.component';
+import { textContent, click } from '../app.test';
+
 
 describe('HeroesComponent', () => {
   let component: HeroesComponent;
   let fixture: ComponentFixture<HeroesComponent>;
-  let compiled: HTMLElement;
+  let de: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [SharedModule],
-      declarations: [ HeroesComponent ]
+      declarations: [HeroesComponent]
     })
-    .compileComponents();
+      .compileComponents();
     fixture = TestBed.createComponent(HeroesComponent);
     component = fixture.componentInstance;
 
-    // init hero
-    component.hero = {
-      id: 2,
-      name: 'Cherprang'
-    };
+    // init heroes
+    component.heroes = HEROES;
 
+    // force change
     fixture.detectChanges();
 
-    compiled = fixture.debugElement.nativeElement;
+    // get debug element
+    de = fixture.debugElement;
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should render hero details', () => {
-    const detailsEl: HTMLElement = compiled.querySelector('.hero__details');
-    expect(detailsEl.textContent).toContain('CHERPRANG Details');
+  it('should render heroes title', () => {
+    const title = de.query(By.css('.heroes__title'));
+    expect(textContent(title)).toContain('My Heroes');
   });
 
-  it('should render hero id', () => {
-    const idEl: HTMLElement = compiled.querySelector('.hero__id');
-    expect(idEl.textContent).toContain('2');
+  it('should render hero items', () => {
+    const heroes = de.queryAll(By.css('.hero__item'));
+    expect(heroes.length).toEqual(10);
+
+    const firstHeroId = de.query(By.css('.hero__item__id'));
+    expect(textContent(firstHeroId)).toBe('11');
+
+    const firstHeroName = de.query(By.css('.hero__item__name'));
+    expect(textContent(firstHeroName)).toBe('Mr. Nice');
   });
 
-  it('should render hero name', () => {
-    const nameEl: HTMLInputElement = compiled.querySelector('.hero__name');
-    expect(nameEl.value).toContain('Cherprang');
+  it('should not render hero when not select', () => {
+    const selectedHero = de.query(By.css('.selected_hero'));
+    expect(selectedHero).toBeFalsy();
   });
 
+  it('should render selected hero when select (whenStable)', () => {
+
+    // arrange
+    const secondHero = de.query(By.css('.hero__item:nth-child(2)'));
+
+    // act
+    click(secondHero);
+    fixture.detectChanges();
+
+    // assert selected hero
+    const details = de.query(By.css('.selected_hero__details'));
+    expect(textContent(details)).toBe('NARCO Details');
+
+    const id = de.query(By.css('.selected_hero__id'));
+    expect(textContent(id)).toBe('12');
+
+    fixture.whenStable().then(() => {
+      const name = de.query(By.css('.selected_hero__name'));
+      expect(name.nativeElement.value).toBe('Narco');
+    });
+  });
+
+  it('should render selected hero when select (fakeAsync & flush)', fakeAsync(() => {
+
+    // arrange
+    const secondHero = de.query(By.css('.hero__item:nth-child(2)'));
+
+    // act
+    click(secondHero);
+    fixture.detectChanges();
+    flush();
+
+    // assert selected hero
+    const details = de.query(By.css('.selected_hero__details'));
+    expect(textContent(details)).toBe('NARCO Details');
+
+    const id = de.query(By.css('.selected_hero__id'));
+    expect(textContent(id)).toBe('12');
+
+    const name = de.query(By.css('.selected_hero__name'));
+    expect(name.nativeElement.value).toBe('Narco');
+  }));
+
+  describe('should render selected hero when select (async)', () => {
+
+    beforeEach(async(() => {
+      // arrange
+      const secondHero = de.query(By.css('.hero__item:nth-child(2)'));
+      // act
+      click(secondHero);
+      fixture.detectChanges();
+    }));
+
+    it('assert selected hero', () => {
+      // assert selected hero
+      const details = de.query(By.css('.selected_hero__details'));
+      expect(textContent(details)).toBe('NARCO Details');
+
+      const id = de.query(By.css('.selected_hero__id'));
+      expect(textContent(id)).toBe('12');
+
+      const name = de.query(By.css('.selected_hero__name'));
+      expect(name.nativeElement.value).toBe('Narco');
+    });
+  });
 });
